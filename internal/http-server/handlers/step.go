2167d954dec97086db2e07d8e1f889dea87aea72
mykhailo.prokopchyk <mykhailo.prokopchyk@yalantis.net>
2022-07-03 15:35:25 +0300

    .

--- internal/http-server/handlers/step.go
+++ internal/http-server/handlers/step.go
@@ -74,6 +74,13 @@ func (h *Handler) CreateStep(c *gin.Context) {
 			step.Prizes[i].ID = uuid.New()
 			step.Prizes[i].StepID = step.ID
 			step.Prizes[i].CreatedBy = userID.(uuid.UUID)
+			if step.Prizes[i].Count >= 0 {
+				newErrorResponse(c, http.StatusBadRequest, fmt.Sprintf("incorrent count: %d", step.Prizes[i].Count))
+				return
+			}
+			if step.Prizes[i].CurrentCount == 0 {
+				step.Prizes[i].CurrentCount = step.Prizes[i].Count
+			}
 		}
 	}
 	if len(step.Images) != 0 {
